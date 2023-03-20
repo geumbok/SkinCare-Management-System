bbed1a23eaa352a8f119e444b0870c6985b95cfb
geumbok <dltmdwns3927@naver.com>
2023-03-20 23:08:19 +0900

    removed menu.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Edit SkinCare");
+		System.out.println("2. Effect SkinCare");
+		System.out.println("3. Price SkinCare");
+		System.out.println("4. View SkinCare");
+		System.out.println("5. Exit");
+
+	} 
+
+}
